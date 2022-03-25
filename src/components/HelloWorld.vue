5eef1a77d7a4e9bf7d0dbafda39171fe02b4f28c
Adiel <you@example.com>
2022-03-25 15:04:14 +0300

    text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,16 +12,11 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Quickly Solve
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Great system for connecting solution to students!
         </p>
       </v-col>
 
