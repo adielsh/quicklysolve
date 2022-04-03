397aa8547a4f18b1e316317661ece756918571fc
adielsh <adielvl@gmail.com>
2022-04-03 14:10:01 +0300

    4 files modified

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-     <v-row style="margin-top:60px">
+    <v-row style="margin-top: 60px">
       <v-col cols="12" sm="12" offset-sm="12">
         <v-card class="wrap-cards">
           <v-container fluid>
@@ -11,18 +11,28 @@
                 class="d-flex child-flex"
                 cols="4"
               >
-                <v-card style="height:200px;width:200px;" elevation="9" shaped outlined flat tile class="d-flex">
-                  <v-card-text >
-                      <div>{{user.data.name}}</div>
-                      <div>{{user.id}}</div>
-                      <div>{{user.data.type}}</div>
-                      <div v-if="user.data.tags">
-                               <v-chip v-for="(t,i) in user.data.tags" :key="i"
-                                  :color="getRandomColor()"
-                                  outlined
-                                >{{t.label}}</v-chip>
-                      </div>
-                     
+                <v-card
+                  style="height: 200px; width: 200px"
+                  elevation="9"
+                  shaped
+                  outlined
+                  flat
+                  tile
+                  class="d-flex"
+                >
+                  <v-card-text>
+                    <div>{{ user.data.name }}</div>
+                    <div>{{ user.id }}</div>
+                    <div>{{ user.data.type }}</div>
+                    <div v-if="user.data.tags">
+                      <v-chip
+                        v-for="(t, i) in user.data.tags"
+                        :key="i"
+                        :color="getRandomColor()"
+                        outlined
+                        >{{ t.label }}</v-chip
+                      >
+                    </div>
                   </v-card-text>
                 </v-card>
               </v-col>
@@ -30,43 +40,45 @@
           </v-container>
         </v-card>
       </v-col>
-     </v-row>
+    </v-row>
   </v-container>
 </template>
 
-
 <script>
-import * as Firebase from "../firebase_config";
+import * as Firebase from '../firebase_config';
 export default {
-   data: () => ({
-     users :[]
-   }),
+  data: () => ({
+    users: [],
+  }),
   async mounted() {
+    this.getAllTags();
+
     Firebase.db
-      .collection("users")
-      .where("type", "==","TEACHER" )
+      .collection('users')
+      .where('type', '==', 'TEACHER')
       .onSnapshot((querySnapshot) => {
         this.users = [];
         // console.log(querySnapshot)
         querySnapshot.forEach((doc) => {
-         
-          console.log(doc.data())
-          if(!doc.data().tags){
-              this.users.push({id: doc.id ,data : doc.data()})
-              return;
-          } 
-          var tags = []
-          doc.data().tags.forEach((val,key,arr)=>{
-          Firebase.db
-           .collection("tags").doc(val).get().then(result=>{
-             tags.push(result.data())
-             if (Object.is(arr.length - 1, key)) {
-                  let o ={id: doc.id ,data : doc.data()}
-                  o.data.tags = tags
-                  this.users.push(o)
-             } 
-            })
-
+          console.log(doc.data());
+          if (!doc.data().tags) {
+            this.users.push({ id: doc.id, data: doc.data() });
+            return;
+          }
+          var tags = [];
+          doc.data().tags.forEach((val, key, arr) => {
+            Firebase.db
+              .collection('tags')
+              .doc(val)
+              .get()
+              .then((result) => {
+                tags.push(result.data());
+                if (Object.is(arr.length - 1, key)) {
+                  let o = { id: doc.id, data: doc.data() };
+                  o.data.tags = tags;
+                  this.users.push(o);
+                }
+              });
 
             //  Firebase.db
             //  .collection("tags")
@@ -74,23 +86,31 @@ export default {
             //  .get().then(result=>{
             //   console.log(result)
             //  })
-           
-          })
+          });
         });
       });
   },
-  methods:{
-    getRandomColor(){
-      const colors = ["red", "blue", "orange", "green","yellow","purple","brown","black"];
+  methods: {
+    getAllTags() {
+      //todo: get all tags for db
+    },
+    getRandomColor() {
+      const colors = [
+        'red',
+        'blue',
+        'orange',
+        'green',
+        'yellow',
+        'purple',
+        'brown',
+        'black',
+      ];
 
       const random = Math.floor(Math.random() * colors.length);
-      return colors[random]
-    }
-  }
-
-}
+      return colors[random];
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
