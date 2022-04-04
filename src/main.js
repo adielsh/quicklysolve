7fa8524e0b672bda7e9cb0d822ec9def55f30a18
adielsh <adielvl@gmail.com>
2022-04-04 10:27:46 +0300

    1 file modified

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import { Toast } from "toaster-js"; // const { Toast } = require("toaster-js/umd.js");
-import "toaster-js/default.scss"; // Assuming CSS/SCSS loader is present
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import { Toast } from 'toaster-js'; // const { Toast } = require("toaster-js/umd.js");
+import 'toaster-js/default.scss'; // Assuming CSS/SCSS loader is present
 // Import styles from SCSS: @import "../node_modules/toaster-js/default.scss";
-// Or just copy default styles to your project from node_modules/toaster-js/default.*css. 
+// Or just copy default styles to your project from node_modules/toaster-js/default.*css.
 // Or draw your own styles! For instance, you can make toasts appear on the left by overwriting .toast { left: 0; right: auto; }
- 
-// Simple toast
-new Toast("Welcome!");
-Vue.config.productionTip = false
 
+// Simple toast
+Vue.config.productionTip = false;
+
+new Toast('עדכון בוצע', Toast.TYPE_SUCCESS, Toast.TIME_SHORT);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
